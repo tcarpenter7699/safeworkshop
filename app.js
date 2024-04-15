0d10246e9216538fdf8238060f127b70b92670be
tcarpenter7699 <142123960+tcarpenter7699@users.noreply.github.com>
2024-04-15 12:40:40 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+//Pseudocode//
+//Assign variables//
+//Create dialogue box with the given text//
+//create the function to generate the combination//
+
+
+
+// creating app variables
+const one = 10;
+const two = 40;
+const three = 39;
+//creating dialogue box//
+let combination = prompt("You have received this message because you have been chosen to open an important vault. Here is the secret combination:");
+//creating mathematic function that will generate combination
+console.log((two/4)+"-"+(three+1)+"-"+(two-1));
+
